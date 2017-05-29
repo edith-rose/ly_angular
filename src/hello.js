4a304215ec6e5fadb0bbe0578cdb9f6d24880ee1
edith-rose <271664934@qq.com>
2017-05-29 11:25:15 +0800

    【环境搭建】1.karma添加karma-browserify插件，2.package.json添加jquery和lodash，3.修改hello.js例子

--- src/hello.js
+++ src/hello.js
@@ -1,12 +1,5 @@
-function sayHello() {
-  ly(1,3);
-  return "Hello, world!";
-}
+var _ = require('lodash');
 
-function ly(num1, num2) {
-  if (num1 % 2 == 1) {
-    return num1 + num2;
-  } else {
-    return num1 * num2;
-  }
-}
+module.exports = function sayHello(to) {
+  return _.template('Hello, <%= name%>!')({name: to});
+};
--- test/hello_spec.js
+++ test/hello_spec.js
@@ -1,14 +1,8 @@
+var sayHello = require('../src/hello');
+
 describe('hello', function() {
 
   it('says hello luoying', function() {
-    expect(sayHello()).toBe('Hello, world!');
-  });
-
-  it('输入两个数，第一个数字为奇数时，两个数字相加', function() {
-    expect(ly(1, 3)).toBe(4);
-  });
-
-  it('输入两个数，第一个数字为偶数时，两个数字相乘', function() {
-    expect(ly(2, 4)).toBe(8);
+    expect(sayHello('Jane')).toBe('Hello, Jane!');
   });
 });
